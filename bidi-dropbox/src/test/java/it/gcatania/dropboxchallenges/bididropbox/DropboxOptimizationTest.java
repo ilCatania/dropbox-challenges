64c9a755686fb2ebfbed550d809d3132975c864d
Gabriele Catania <gabriele.ctn@gmail.com>
2011-02-04 11:22:01 +0100

    reworked optimization test to provide more information

--- bidi-dropbox/src/test/java/it/gcatania/dropboxchallenges/bididropbox/DropboxOptimizationTest.java
+++ bidi-dropbox/src/test/java/it/gcatania/dropboxchallenges/bididropbox/DropboxOptimizationTest.java
@@ -1,6 +1,5 @@
 package it.gcatania.dropboxchallenges.bididropbox;
 
-import it.gcatania.dropboxchallenges.bididropbox.comparators.DropBoxAreaComparator;
 import it.gcatania.dropboxchallenges.bididropbox.comparators.RectangleAreaComparator;
 import it.gcatania.dropboxchallenges.bididropbox.comparators.RectangleMaxSideComparator;
 import it.gcatania.dropboxchallenges.bididropbox.comparators.RectanglePerimeterComparator;
@@ -13,6 +12,7 @@ import it.gcatania.dropboxchallenges.bididropbox.overheadcalculators.DropBoxOver
 import it.gcatania.dropboxchallenges.bididropbox.overheadcalculators.FreeSpaceOverheadCalculator;
 import it.gcatania.dropboxchallenges.bididropbox.overheadcalculators.OverheadCalculator;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -31,7 +31,7 @@ import org.testng.annotations.Test;
 public class DropboxOptimizationTest
 {
 
-    private static final int NUM_ITERATIONS = 100;
+    private static final int NUM_ITERATIONS = 1080;
 
     private static final int NUM_RECTANGLES = 200;
 
@@ -39,53 +39,219 @@ public class DropboxOptimizationTest
 
     private static final int MAX_SIDE_LENGTH = 30;
 
+    private Map<Setup, Score> optimizationData;
+
+    private static final class Setup
+    {
+
+        private final Comparator<Rectangle> comparator;
+
+        private final OverheadCalculator calculator;
+
+        private Setup(Comparator<Rectangle> comparator, OverheadCalculator calculator)
+        {
+            this.calculator = calculator;
+            this.comparator = comparator;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (obj instanceof Setup)
+            {
+                Setup other = (Setup) obj;
+                // using by-reference equality, won't work outside this class!
+                return other.comparator == comparator && other.calculator == calculator;
+            }
+            return false;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int hashCode()
+        {
+            // don't care about null safety here
+            return 1 + comparator.hashCode() + 13 * calculator.hashCode();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString()
+        {
+            return new StringBuilder(comparator.getClass().getSimpleName())
+                .append(' ')
+                .append(calculator.getClass().getSimpleName())
+                .toString();
+        }
+    }
+
+    private static final class Score
+    {
+
+        private int firstPlaces = 0;
+
+        private int totalArea = 0;
+
+        private int totalFreeSpace = 0;
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString()
+        {
+            return new StringBuilder("First places: ")
+                .append(firstPlaces)
+                .append("; total area: ")
+                .append(totalArea)
+                .append("; total free space: ")
+                .append(totalFreeSpace)
+                .toString();
+        }
+    }
+
     @Test
-    public void testOptimization()
+    public void testOptimization() throws IOException
     {
         // 133l
         Random random = new Random();
         random.setSeed(133l);
 
-        Map<String, Integer> results = new HashMap<String, Integer>();
+        @SuppressWarnings("unchecked")
+        List<Comparator<Rectangle>> comparators = Arrays.asList(
+            new RectangleAreaComparator(),
+            new RectangleMaxSideComparator(),
+            new RectanglePerimeterComparator());
+        List<OverheadCalculator> overheadCalculators = Arrays.asList(
+            new AreaOverheadCalculator(),
+            new DistanceFromOriginOverheadCalculator(),
+            new DropBoxAreaOverheadCalculator(),
+            new FreeSpaceOverheadCalculator(),
+            new DropBoxOverheadCalculator());
+        optimizationData = new HashMap<Setup, Score>();
         for (int i = 0; i < NUM_ITERATIONS; i++)
         {
             System.out.println("pass " + i);
-            @SuppressWarnings("unchecked")
-            List<String> bestStrategies = singlePass(random, Arrays.asList(
-                new RectangleAreaComparator(),
-                new RectangleMaxSideComparator(),
-                new RectanglePerimeterComparator()), Arrays.asList(
-                new AreaOverheadCalculator(),
-                new DistanceFromOriginOverheadCalculator(),
-                new DropBoxAreaOverheadCalculator(),
-                new FreeSpaceOverheadCalculator(),
-                new DropBoxOverheadCalculator()));
-            for (String best : bestStrategies)
+            singlePass(random, comparators, overheadCalculators);
+        }
+        List<Map.Entry<Setup, Score>> entrySet = new ArrayList<Map.Entry<Setup, Score>>(optimizationData.entrySet());
+        Collections.sort(entrySet, new Comparator<Map.Entry<Setup, Score>>()
+        {
+
+            @Override
+            public int compare(Map.Entry<Setup, Score> e1, Map.Entry<Setup, Score> e2)
             {
-                Integer count = results.get(best);
-                if (count == null)
+                Score score1 = e1.getValue();
+                Score score2 = e2.getValue();
+                int firstPlacesDiff = score2.firstPlaces - score1.firstPlaces;
+                if (firstPlacesDiff == 0)
                 {
-                    count = 1;
+                    int totalAreaDiff = score1.totalArea - score2.totalArea;
+                    if (totalAreaDiff == 0)
+                    {
+                        return score1.totalFreeSpace - score2.totalFreeSpace;
+                    }
+                    return totalAreaDiff;
                 }
-                else
-                {
-                    count = count + 1;
-                }
-                results.put(best, count);
+                return firstPlacesDiff;
+            }
+        });
+
+        int totalFirstPlaces = 0;
+        int minArea = Integer.MAX_VALUE;
+        int maxFreeSpace = 0;
+        for (Map.Entry<Setup, Score> e : entrySet)
+        {
+            Score score = e.getValue();
+            totalFirstPlaces += score.firstPlaces;
+            if (minArea > score.totalArea)
+            {
+                minArea = score.totalArea;
+            }
+            if (maxFreeSpace < score.totalFreeSpace)
+            {
+                maxFreeSpace = score.totalFreeSpace;
             }
         }
-        System.out.println("results:\n");
-        for (Map.Entry<String, Integer> e : results.entrySet())
+
+        System.out
+            .println("Comparator;Calculator;FirstPlaces;FirstPlacesPerc;TotalArea;TotalAreaPerc;FreeSpace;FreeSpacePerc");
+        for (Map.Entry<Setup, Score> e : entrySet)
         {
-            int value = e.getValue();
-            System.out.println(e.getKey() + ": " + value + " (" + (value * 100 / NUM_ITERATIONS) + "%)");
+            Setup setup = e.getKey();
+            Score score = e.getValue();
+            int firstPlacesPerc = 100 * score.firstPlaces / totalFirstPlaces;
+            int totalAreaPerc = 100 * score.totalArea / minArea;
+            int freeSpacePerc = 100 * score.totalFreeSpace / maxFreeSpace;
+            System.out.println(new StringBuilder(setup.comparator.getClass().getSimpleName())
+                .append(';')
+                .append(setup.calculator.getClass().getSimpleName())
+                .append(score.firstPlaces)
+                .append(';')
+                .append(firstPlacesPerc)
+                .append(';')
+                .append(score.totalArea)
+                .append(';')
+                .append(totalAreaPerc)
+                .append(';')
+                .append(score.totalFreeSpace)
+                .append(';')
+                .append(freeSpacePerc)
+                .toString());
         }
     }
 
-    private List<String> singlePass(Random random, List<Comparator<Rectangle>> comparators,
+    private void singlePass(Random random, List<Comparator<Rectangle>> comparators,
         List<OverheadCalculator> overheadCalculators)
     {
 
+        List<Rectangle> rectangles = createRandomRectangles(random);
+
+        int minArea = Integer.MAX_VALUE;
+        List<Score> minAreaScores = new ArrayList<Score>();
+        for (Comparator<Rectangle> comp : comparators)
+        {
+            for (OverheadCalculator o : overheadCalculators)
+            {
+                DropBoxBuilder builder = new DropBoxBuilder(comp, o);
+                DropBox dropBox = builder.build(rectangles);
+                int currentArea = dropBox.getArea();
+
+                Score score = optimizationData.get(new Setup(comp, o));
+                if (score == null)
+                {
+                    score = new Score();
+                    optimizationData.put(new Setup(comp, o), score);
+                }
+                score.totalArea += currentArea;
+                score.totalFreeSpace += dropBox.getFreeSpace();
+                if (currentArea < minArea)
+                {
+                    minArea = currentArea;
+                    minAreaScores.clear();
+                    minAreaScores.add(score);
+                }
+                else if (currentArea == minArea)
+                {
+                    minAreaScores.add(score);
+                }
+            }
+        }
+        for (Score score : minAreaScores)
+        {
+            score.firstPlaces++;
+        }
+    }
+
+    private static List<Rectangle> createRandomRectangles(Random random)
+    {
         List<Rectangle> rectangles = new ArrayList<Rectangle>(NUM_RECTANGLES);
         for (int i = 0; i < NUM_RECTANGLES; i++)
         {
@@ -95,32 +261,7 @@ public class DropboxOptimizationTest
             // System.out.println("Adding random " + rect);
             rectangles.add(rect);
         }
-
-        Map<DropBox, String> dropBoxesToUsed = new HashMap<DropBox, String>(comparators.size()
-            * overheadCalculators.size(), 1f);
-        for (Comparator<Rectangle> comp : comparators)
-        {
-            for (OverheadCalculator o : overheadCalculators)
-            {
-                DropBoxBuilder dbb = new DropBoxBuilder(comp, o);
-                DropBox result = dbb.build(rectangles);
-                String toolsUsed = comp.getClass().getSimpleName() + ";" + o.getClass().getSimpleName();
-                dropBoxesToUsed.put(result, toolsUsed);
-            }
-        }
-        List<DropBox> dropBoxes = new ArrayList<DropBox>(dropBoxesToUsed.keySet());
-        Collections.sort(dropBoxes, new DropBoxAreaComparator());
-        DropBox best = dropBoxes.get(0);
-        int bestArea = best.getArea();
-        List<String> result = new ArrayList<String>();
-        for (DropBox db : dropBoxes)
-        {
-            if (db.getArea() == bestArea)
-            {
-                result.add(dropBoxesToUsed.get(db));
-            }
-        }
-        return result;
+        return rectangles;
     }
 
 }
