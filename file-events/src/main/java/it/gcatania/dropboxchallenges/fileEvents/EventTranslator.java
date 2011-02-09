80455ac26255688414261733200eeb1bda6afb06
Gabriele Catania <gabriele.ctn@gmail.com>
2011-02-09 14:50:02 +0100

    adding file-events project

---
+++ file-events/src/main/java/it/gcatania/dropboxchallenges/fileEvents/EventTranslator.java
@@ -0,0 +1,14 @@
+package it.gcatania.dropboxchallenges.fileEvents;
+
+/**
+ * @author gcatania
+ */
+public class EventTranslator
+{
+
+    public static void main(String[] args)
+    {
+        // TODO
+    }
+
+}
