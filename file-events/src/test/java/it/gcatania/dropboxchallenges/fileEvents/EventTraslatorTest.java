d07677e7a89c36d468ffbacfdf769b8fef600b3c
Gabriele Catania <gabriele.ctn@gmail.com>
2011-02-15 12:19:03 +0100

    reworked unit test

--- file-events/src/test/java/it/gcatania/dropboxchallenges/fileEvents/EventTraslatorTest.java
+++ file-events/src/test/java/it/gcatania/dropboxchallenges/fileEvents/EventTraslatorTest.java
@@ -5,8 +5,9 @@ import it.gcatania.dropboxchallenges.fileEvents.model.RawEventType;
 import it.gcatania.dropboxchallenges.fileEvents.model.structured.CreationEvent;
 import it.gcatania.dropboxchallenges.fileEvents.model.structured.StructuredEvent;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import org.testng.Assert;
@@ -24,13 +25,25 @@ public class EventTraslatorTest
     @Test
     public void testFileAdd1()
     {
-
-        List<RawEvent> raw = new ArrayList<RawEvent>();
-
-        raw.add(new RawEvent(RawEventType.ADD, 0, "/file1.txt", "0"));
-
+        List<RawEvent> raw = make(new RawEvent(RawEventType.ADD, 0, "/file1.txt", "0"));
         List<StructuredEvent> result = translator.parseMessages(raw);
-        Assert.assertEquals(result, Arrays.<StructuredEvent> asList(new CreationEvent("/file1.txt", "0")));
+        check(result, new CreationEvent("/file1.txt", "0"));
+    }
+
+    private static List<RawEvent> make(RawEvent... events)
+    {
+        return Arrays.asList(events);
+    }
+
+    private static void check(Collection<StructuredEvent> result, StructuredEvent... expectedEvents)
+    {
+        Assert.assertEquals(result.size(), expectedEvents.length);
+        Iterator<StructuredEvent> iter = result.iterator();
+        for (StructuredEvent expected : expectedEvents)
+        {
+            StructuredEvent actual = iter.next();
+            Assert.assertEquals(actual, expected);
+        }
     }
 
 }
